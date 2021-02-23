83d5224516e036c6f47653f2c215360f38d93388
Vijay_Ky <vijayky007@gmail.com>
2021-02-23 11:16:34 +0530

    updated

---
+++ Annotations/src/app1/M19.java
@@ -0,0 +1,14 @@
+package app1;
+
+import java.util.ArrayList;
+
+class M19
+{
+	public static void main(String[] args)
+	{
+		ArrayList list = new ArraList();
+		list.add(90);
+		list.add(90);
+		list.add(900);
+	}
+}
