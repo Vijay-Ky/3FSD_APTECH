4bfe03fbd4bab9d45e35b04067efd90f2ba1c1f1
Vijay_Ky <vijayky007@gmail.com>
2021-02-23 14:18:57 +0530

    updated

--- Annotations/app1/M19.java
+++ Annotations/app1/M19.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 
 class M19
 {
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args)
 	{
-		ArrayList list = new ArraList();
+		ArrayList list = new ArrayList();
 		list.add(90);
 		list.add(90);
 		list.add(900);
